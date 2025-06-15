38eafbd80faf07d08b27117581c28a8b711cbd56
AlvaroDiazTorres <90872843+AlvaroDiazTorres@users.noreply.github.com>
2025-06-15 11:23:07 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -17,7 +17,7 @@ app.use(cookieParser())
 
 //cambiar la url cuando deploy
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://playground-front-five.vercel.app/', '83.37.32.61:5173'],
+    origin: ['http://localhost:5173', 'https://playground-front-five.vercel.app', '83.37.32.61:5173'],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
